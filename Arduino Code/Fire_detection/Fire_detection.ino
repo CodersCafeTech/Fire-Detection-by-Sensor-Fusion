024639cc961c26248e5686b0b1ad4280c180b8e1
Nekhil-haxh <53958279+Nekhil-haxh@users.noreply.github.com>
2022-12-21 21:27:27 +0530

    Add files via upload

---
+++ Arduino Code/Fire_detection/Fire_detection.ino
@@ -0,0 +1,281 @@
+
+/* Includes ---------------------------------------------------------------- */
+#include <Fire_Detection_with_sensor_fusion_inferencing.h>
+#include <Arduino_LPS22HB.h> //Click here to get the library: http://librarymanager/All#Arduino_LPS22HB
+#include <Arduino_HTS221.h> //Click here to get the library: http://librarymanager/All#Arduino_HTS221
+
+
+enum sensor_status 
+{
+    NOT_USED = -1,
+    NOT_INIT,
+    INIT,
+    SAMPLED
+};
+
+/** Struct to link sensor axis name to sensor value function */
+
+typedef struct
+{
+    const char *name;
+    float *value;
+    uint8_t (*poll_sensor)(void);
+    bool (*init_sensor)(void);    
+    sensor_status status;
+} eiSensors;
+
+/* Constant defines -------------------------------------------------------- */
+#define CONVERT_G_TO_MS2    9.80665f
+#define MAX_ACCEPTED_RANGE  2.0f        // starting 03/2022, models are generated setting range to +-2,
+                                        // but this example use Arudino library which set range to +-4g. 
+                                        // If you are using an older model, ignore this value and use 4.0f instead
+/** Number sensor axes used */
+#define N_SENSORS     18
+
+/* Forward declarations ------------------------------------------------------- */
+float ei_get_sign(float number);
+
+bool init_HTS(void);
+bool init_BARO(void);
+
+uint8_t poll_HTS(void);
+uint8_t poll_BARO(void);
+
+/* Private variables ------------------------------------------------------- */
+static const bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+
+static float data[N_SENSORS];
+static bool ei_connect_fusion_list(const char *input_list);
+
+static int8_t fusion_sensors[N_SENSORS];
+static int fusion_ix = 0;
+
+/** Used sensors value function connected to label name */
+eiSensors sensors[] =
+{
+
+    "temperature", &data[9], &poll_HTS, &init_HTS, NOT_USED,
+    "humidity", &data[10], &poll_HTS, &init_HTS, NOT_USED,
+    "pressure", &data[11], &poll_BARO, &init_BARO, NOT_USED,
+};
+
+/**
+* @brief      Arduino setup function
+*/
+void setup()
+{
+    /* Init serial */
+    Serial.begin(115200);
+    // comment out the below line to cancel the wait for USB connection (needed for native USB)
+    while (!Serial);
+    Serial.println("Edge Impulse Sensor Fusion Inference\r\n");
+
+    /* Connect used sensors */
+    if(ei_connect_fusion_list(EI_CLASSIFIER_FUSION_AXES_STRING) == false) {
+        ei_printf("ERR: Errors in sensor list detected\r\n");
+        return;
+    }
+
+    /* Init & start sensors */
+
+    for(int i = 0; i < fusion_ix; i++) {
+        if (sensors[fusion_sensors[i]].status == NOT_INIT) {
+            sensors[fusion_sensors[i]].status = (sensor_status)sensors[fusion_sensors[i]].init_sensor();
+            if (!sensors[fusion_sensors[i]].status) {
+              ei_printf("%s axis sensor initialization failed.\r\n", sensors[fusion_sensors[i]].name);             
+            }
+            else {
+              ei_printf("%s axis sensor initialization successful.\r\n", sensors[fusion_sensors[i]].name);
+            }
+        }
+    }
+}
+
+/**
+* @brief      Get data and run inferencing
+*/
+void loop()
+{
+    ei_printf("\nStarting inferencing in 2 seconds...\r\n");
+
+    delay(2000);
+
+    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != fusion_ix) {
+        ei_printf("ERR: Sensors don't match the sensors required in the model\r\n"
+        "Following sensors are required: %s\r\n", EI_CLASSIFIER_FUSION_AXES_STRING);
+        return;
+    }
+
+    ei_printf("Sampling...\r\n");
+
+    // Allocate a buffer here for the values we'll read from the sensor
+    float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
+
+    for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME) {
+        // Determine the next tick (and then sleep later)
+        int64_t next_tick = (int64_t)micros() + ((int64_t)EI_CLASSIFIER_INTERVAL_MS * 1000);
+
+        for(int i = 0; i < fusion_ix; i++) {
+            if (sensors[fusion_sensors[i]].status == INIT) {
+                sensors[fusion_sensors[i]].poll_sensor();
+                sensors[fusion_sensors[i]].status = SAMPLED;
+            }
+            if (sensors[fusion_sensors[i]].status == SAMPLED) {
+                buffer[ix + i] = *sensors[fusion_sensors[i]].value;
+                sensors[fusion_sensors[i]].status = INIT;
+            }
+        }
+
+        int64_t wait_time = next_tick - (int64_t)micros();
+
+        if(wait_time > 0) {
+            delayMicroseconds(wait_time);
+        }
+    }
+
+    // Turn the raw buffer in a signal which we can the classify
+    signal_t signal;
+    int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+    if (err != 0) {
+        ei_printf("ERR:(%d)\r\n", err);
+        return;
+    }
+
+    // Run the classifier
+    ei_impulse_result_t result = { 0 };
+
+    err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        ei_printf("ERR:(%d)\r\n", err);
+        return;
+    }
+
+    // print the predictions
+    ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d ms.):\r\n",
+        result.timing.dsp, result.timing.classification, result.timing.anomaly);
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) 
+    {
+        ei_printf("%s: %.5f\r\n", result.classification[ix].label, result.classification[ix].value);
+        if(result.classification[ix].value > 0.8 )
+        {
+          String Label = String(result.classification[ix].label);
+          Serial.println(Label);
+          if (Label == "Fire")
+              {
+                Serial.println("Fire Detected ");
+                Serial.println("Going to alert!!! ");
+              }
+        }
+    }
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    ei_printf("    anomaly score: %.3f\r\n", result.anomaly);
+#endif
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || (EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_FUSION && EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_ACCELEROMETER)
+#error "Invalid model for current sensor"
+#endif
+
+
+/**
+ * @brief Go through sensor list to find matching axis name
+ *
+ * @param axis_name
+ * @return int8_t index in sensor list, -1 if axis name is not found
+ */
+static int8_t ei_find_axis(char *axis_name)
+{
+    int ix;
+    for(ix = 0; ix < N_SENSORS; ix++) {
+        if(strstr(axis_name, sensors[ix].name)) {
+            return ix;
+        }
+    }
+    return -1;
+}
+
+/**
+ * @brief Check if requested input list is valid sensor fusion, create sensor buffer
+ *
+ * @param[in]  input_list      Axes list to sample (ie. "accX + gyrY + magZ")
+ * @retval  false if invalid sensor_list
+ */
+static bool ei_connect_fusion_list(const char *input_list)
+{
+    char *buff;
+    bool is_fusion = false;
+
+    /* Copy const string in heap mem */
+    char *input_string = (char *)ei_malloc(strlen(input_list) + 1);
+    if (input_string == NULL) {
+        return false;
+    }
+    memset(input_string, 0, strlen(input_list) + 1);
+    strncpy(input_string, input_list, strlen(input_list));
+
+    /* Clear fusion sensor list */
+    memset(fusion_sensors, 0, N_SENSORS);
+    fusion_ix = 0;
+
+    buff = strtok(input_string, "+");
+
+    while (buff != NULL) { /* Run through buffer */
+        int8_t found_axis = 0;
+
+        is_fusion = false;
+        found_axis = ei_find_axis(buff);
+
+        if(found_axis >= 0) {
+            if(fusion_ix < N_SENSORS) {
+                fusion_sensors[fusion_ix++] = found_axis;
+                sensors[found_axis].status = NOT_INIT;
+            }
+            is_fusion = true;
+        }
+
+        buff = strtok(NULL, "+ ");
+    }
+
+    ei_free(input_string);
+
+    return is_fusion;
+}
+
+/**
+ * @brief Return the sign of the number
+ * 
+ * @param number 
+ * @return int 1 if positive (or 0) -1 if negative
+ */
+float ei_get_sign(float number) {
+    return (number >= 0.0) ? 1.0 : -1.0;
+}
+
+bool init_HTS(void) {
+  static bool init_status = false;
+  if (!init_status) {
+    init_status = HTS.begin();
+  }
+  return init_status;
+}
+
+bool init_BARO(void) {
+  static bool init_status = false;
+  if (!init_status) {
+    init_status = BARO.begin();
+  }
+  return init_status;
+}
+
+uint8_t poll_HTS(void) {
+  
+    data[9] = HTS.readTemperature();
+    data[10] = HTS.readHumidity();
+    return 0;
+}
+
+uint8_t poll_BARO(void) {
+  
+    data[11] = BARO.readPressure(); // (PSI/MILLIBAR/KILOPASCAL) default kPa
+    return 0;
+}
